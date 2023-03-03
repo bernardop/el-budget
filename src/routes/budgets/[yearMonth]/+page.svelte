0c4958ddd3afae76d146da4bc1f028fa9633edec
Bernardo Pacheco <berna.ipach@gmail.com>
2023-03-03 17:02:23 -0600

    Added fixed and variable expenses cards to template

--- src/routes/budgets/[yearMonth]/+page.svelte
+++ src/routes/budgets/[yearMonth]/+page.svelte
@@ -8,7 +8,17 @@
 		({ entree_type }) => entree_type === 'income'
 	)
 
+	let fixedEntrees = (data.budget?.budget_entrees || []).filter(
+		({ entree_type }) => entree_type === 'fixed_expense'
+	)
+
+	let variableEntrees = (data.budget?.budget_entrees || []).filter(
+		({ entree_type }) => entree_type === 'variable_expense'
+	)
+
 	$: incomeTotal = formatMoney(incomeEntrees.reduce((total, { amount }) => total + amount, 0))
+	$: fixedTotal = formatMoney(fixedEntrees.reduce((total, { amount }) => total + amount, 0))
+	$: variableTotal = formatMoney(variableEntrees.reduce((total, { amount }) => total + amount, 0))
 </script>
 
 <div>
@@ -33,6 +43,44 @@
 						<tr>
 							<td class="bg-accent text-base-100 font-bold">Total</td>
 							<td class="bg-accent text-base-100 font-bold">{incomeTotal}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</article>
+		<article class="card shadow-xl bg-base-300">
+			<div class="card-body">
+				<h2 class="card-title">Fixed Expenses</h2>
+				<table class="table w-full">
+					<tbody>
+						{#each fixedEntrees as entree (entree.id)}
+							<tr>
+								<td>{entree.name}</td>
+								<td>{formatMoney(entree.amount)}</td>
+							</tr>
+						{/each}
+						<tr>
+							<td class="bg-accent text-base-100 font-bold">Total</td>
+							<td class="bg-accent text-base-100 font-bold">{fixedTotal}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</article>
+		<article class="card shadow-xl bg-base-300">
+			<div class="card-body">
+				<h2 class="card-title">Variable Expenses</h2>
+				<table class="table w-full">
+					<tbody>
+						{#each variableEntrees as entree (entree.id)}
+							<tr>
+								<td>{entree.name}</td>
+								<td>{formatMoney(entree.amount)}</td>
+							</tr>
+						{/each}
+						<tr>
+							<td class="bg-accent text-base-100 font-bold">Total</td>
+							<td class="bg-accent text-base-100 font-bold">{variableTotal}</td>
 						</tr>
 					</tbody>
 				</table>
